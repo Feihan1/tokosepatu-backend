6aacae7d4919fc896c0846d9d0a239dee190ca07
lazark kertia <mylazark@gmail.com>
2024-10-18 00:21:48 +0700

    Modif

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -2,29 +2,29 @@ import { SequelizeModuleOptions } from '@nestjs/sequelize';
 import { Dialect } from 'sequelize';
 
 require('dotenv').config();
+
 const sequelizeConfig: SequelizeModuleOptions = {
-  dialect: process.env.DB_DIALECT as Dialect,
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  dialect: process.env.DB_DIALECT as Dialect, // 'postgres'
+  host: process.env.DB_HOST, // Alamat host database
+  port: parseInt(process.env.DB_PORT, 10), // Port database
+  username: process.env.DB_USERNAME, // Username database
+  password: process.env.DB_PASSWORD, // Password database
+  database: process.env.DB_NAME, // Nama database
   autoLoadModels: true,
-  synchronize: true,
+  synchronize: true, // Hati-hati, ini akan menyinkronkan tabel di database
   logging: false,
   sync: {
-    force: false, // WARNING !!!! INI KALAU TRUE AKAN BIKIN ULANG TABEL TIAP REFRESH !!!!
+    force: false, // WARNING: Jika true, tabel akan dihapus dan dibuat ulang setiap kali server dinyalakan
     alter: { drop: true }
   },
   define: {
-    timestamps: true,
-    underscored: true,
+    timestamps: true, // Menggunakan timestamp
+    underscored: true, // Menggunakan format snake_case untuk kolom
   },
-  
 };
+
 export const getJwtSecret = (): string | undefined => {
-  return process.env.JWT_TOKEN;
+  return process.env.JWT_TOKEN; // Mengambil secret token dari variabel lingkungan
 };
 
 export default sequelizeConfig;
-
